2d3ff84eea7008c72caa8e0fe789247d244d8de2
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-13 23:37:57 -0700

    added comments for better understandings

--- src/LongestConsecutive1.java
+++ src/LongestConsecutive1.java
@@ -1,5 +1,9 @@
 public class LongestConsecutive1 {
+
+
     public static int longestConsecutive1(int n){
+
+        // initialy count is 0, also get a max number to store the max count
         int count = 0;
         int max = Integer.MIN_VALUE;
 
@@ -7,11 +11,13 @@ public class LongestConsecutive1 {
             if((n&1) == 1){
                 count ++;
             }
+            //else set the count to 0 and start again
             else{
                 count =0;
-
             }
+            // by moving the n by one movement
             n >>=1;
+            // check the stored value and new count, return the max answer
             max = Math.max(max, count);
         }return max;
 
