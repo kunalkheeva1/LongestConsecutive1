8c1e80cee8eab3e48702e73e6099ea749e7f8bd8
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-13 23:35:58 -0700

    added a method to count the consecutive largest sequence of ones in a bit number

--- src/LongestConsecutive1.java
+++ src/LongestConsecutive1.java
@@ -1,2 +1,24 @@
 public class LongestConsecutive1 {
+    public static int longestConsecutive1(int n){
+        int count = 0;
+        int max = Integer.MIN_VALUE;
+
+        while(n>0){
+            if((n&1) == 1){
+                count ++;
+            }
+            else{
+                count =0;
+
+            }
+            n >>=1;
+            max = Math.max(max, count);
+        }return max;
+
+    }
+
+
+    public static void main(String[] args) {
+
+    }
 }
